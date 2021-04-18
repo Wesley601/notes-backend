68263b72a48222de762f863d8568d69916fa2cfd
wesley601 <wesleypassos601@gmail.com>
2021-04-18 18:11:02 -0300

    Add dto resolver

---
+++ src/DTO/RequestDTOResolver.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\DTO;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Controller\ArgumentValueResolverInterface;
+use Symfony\Component\HttpKernel\ControllerMetadata\ArgumentMetadata;
+use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
+
+class RequestDTOResolver implements ArgumentValueResolverInterface
+{
+    public function __construct(
+        protected ValidatorInterface $validator
+    ) {}
+
+    public function supports(Request $request, ArgumentMetadata $argument)
+    {
+        $reflection = new \ReflectionClass($argument->getType());
+        if ($reflection->implementsInterface(RequestDTOInterface::class)) {
+            return true;
+        }
+
+        return false;
+    }
+
+    public function resolve(Request $request, ArgumentMetadata $argument)
+    {
+        $class = $argument->getType();
+        $dto = new $class($request);
+
+        $errors = $this->validator->validate($dto);
+        if (count($errors) > 0) {
+            throw new BadRequestHttpException((string) $errors);
+        }
+
+        yield $dto;
+    }
+}
